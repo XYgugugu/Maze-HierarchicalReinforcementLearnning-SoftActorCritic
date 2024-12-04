2ffcc4a821816449008b02446bafe96e6c58ca3b
Rzy <Rzy>
2024-12-04 15:54:13 -0600

    12.3

--- Actor_critic.py
+++ Actor_critic.py
@@ -7,15 +7,18 @@ import numpy as np
 class Actor(nn.Module):
     def __init__(self, state_dim, action_dim, max_action):
         super(Actor, self).__init__()
-        self.fc1 = nn.Linear(state_dim, 256)
-        self.fc2 = nn.Linear(256, 256)
+        self.layer1 = nn.Sequential(
+            nn.Linear(state_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU()
+        )
         self.mean = nn.Linear(256, action_dim)
         self.log_std = nn.Linear(256, action_dim)
         self.max_action = max_action
 
     def forward(self, state):
-        x = torch.relu(self.fc1(state))
-        x = torch.relu(self.fc2(x))
+        x = self.layer1(state)
         mean = self.mean(x)
         log_std = self.log_std(x).clamp(-20, 2)  # Bound the log_std for numerical stability
         std = log_std.exp()
@@ -34,16 +37,16 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     def __init__(self, state_dim, action_dim):
         super(Critic, self).__init__()
-        self.fc1 = nn.Linear(state_dim + action_dim, 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.q = nn.Linear(256, 1)
-
+        self.layers = nn.Sequential(
+            nn.Linear(state_dim + action_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1)
+        )
     def forward(self, state, action):
         x = torch.cat([state, action], dim=1)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        q = self.q(x)
-        return q
+        return self.layers(x)
 
 # Soft Actor-Critic
 class SAC:
--- player.py
+++ player.py
@@ -46,6 +46,67 @@ class Player:
         self.score_system.record_pos(self.pos)
         self.score += self.delta_score
 
+    def step(self,action,maze,screen,CELL_SIZE):
+        font = pygame.font.SysFont('Arial', 24)
+        current_time = pygame.time.get_ticks()
+        MOVE_DELAY = 200
+        last_move_time = 0  # 记录上次移动的时间
+    # 只有在上次移动 200 毫秒后才允许再次移动
+        if current_time - last_move_time > MOVE_DELAY:
+            keys = action
+            if keys[pygame.K_UP]:
+                self.move('up', maze)
+                last_move_time = current_time  # 更新上次移动时间
+            if keys[pygame.K_DOWN]:
+                self.move('down', maze)
+                last_move_time = current_time  # 更新上次移动时间
+            if keys[pygame.K_LEFT]:
+                self.move('left', maze)
+                last_move_time = current_time  # 更新上次移动时间
+            if keys[pygame.K_RIGHT]:
+                self.move('right', maze)
+                last_move_time = current_time  # 更新上次移动时间
+
+        # 绘制迷宫和玩家
+        screen.fill((255, 255, 255))  # 背景颜色
+        maze.draw(screen, CELL_SIZE)  # 绘制迷宫
+        self.draw(screen, CELL_SIZE)  # 绘制玩家
+        
+        # 计分板
+        score_text = font.render(f"Score: {self.score}", True, (0,0,0))
+        delta_score_text = font.render(f"Delta Score: {self.delta_score}", True, (0,0,0))
+        screen.blit(score_text, (650, 30))
+        screen.blit(delta_score_text, (650, 70))
+
+        # 检查玩家是否到达下层口
+        if maze.is_prev(self.pos):
+            maze.switch_to_prev_maze()
+            self.score_system.reset_path_tracker()
+            # player = Player(maze.start_pos, score_system=player.score_system)
+            self.tp(maze.start_pos, maze.current_maze_index)
+
+        # 检查玩家是否到达出口
+        if maze.is_exit(self.pos):
+            print("Reached the exit!")
+            if maze.switch_to_next_maze():  # 切换到下一个迷宫 (若没有则退出游戏)
+                self.score_system.reset_path_tracker()
+                self.tp(maze.start_pos, maze.current_maze_index) # 重置玩家位置
+            else:
+                running = False
+        
+        # 检查玩家是否拾取宝石
+        isGem, gem = maze.is_gem(self.pos)
+        if isGem:
+            self.delta_score += self.score_system.action_gem_reward(gem.get_reward(self))
+            self.score += self.delta_score
+            x, y = self.pos
+            maze.grid[x][y] = 0
+
+        # 更新屏幕
+        pygame.display.flip()
+        end_ = False
+        return self.pos, self.delta_score, end_,{}
+
     def tp(self, dest, current_maze_index):
         # 到达新区域 - 加分
         if current_maze_index not in self.discovery:
